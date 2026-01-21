0ec5a2943e66b76568758af4f704f464fddafe2a
rtzeg <82582174+rtzeg@users.noreply.github.com>
2026-01-21 15:07:32 +0500

    Point web container to api service

--- apps/api/apps/users/urls.py
+++ apps/api/apps/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import BotAuthView, LoginView, MeView, ProfileView, RegisterView, VerifyMockView
+from .views import LoginView, MeView, ProfileView, RegisterView, VerifyMockView
 
 urlpatterns = [
     path("auth/register", RegisterView.as_view()),
@@ -8,5 +8,4 @@ urlpatterns = [
     path("auth/me", MeView.as_view()),
     path("profile", ProfileView.as_view()),
     path("profile/verify-mock", VerifyMockView.as_view()),
-    path("bot/auth", BotAuthView.as_view()),
 ]
